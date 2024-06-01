a59e0010b7a4d3ed7b5774bc92caa2332c5bffcc
mattloose <matt.loose@nottingham.ac.uk>
2024-06-01 10:13:10 +0100

    Cleaning up running.

--- src/cnsmeth/theme.py
+++ src/cnsmeth/theme.py
@@ -24,39 +24,58 @@ HEADER_HTML = (Path(__file__).parent / "static" / "header.html").read_text()
 # Read the CSS styles for the application
 STYLE_CSS = (Path(__file__).parent / "static" / "styles.css").read_text()
 
+
 @contextmanager
 def frame(navtitle: str):
     """
     Context manager to create a custom page frame with consistent styling and behavior across all pages.
-    
+
     Args:
         navtitle (str): The title to display in the navigation header.
-    
+
     Yields:
         None
     """
     # Add custom HTML and CSS to the head of the page
     ui.add_head_html(HEADER_HTML + f"<style>{STYLE_CSS}</style>")
-    
+
     # Create a persistent dialog for quitting the app
     with ui.dialog().props("persistent") as quitdialog, ui.card():
-        ui.label("Quitting the app will stop running methylation analysis. Are you sure?")
+        ui.label(
+            "Quitting the app will stop running methylation analysis. Are you sure?"
+        )
         ui.label("If you want to keep analysis running, click Cancel.")
-        ui.label("You can safely close this window and analysis will keep running in the background.")
-        ui.button("Cancel", on_click=quitdialog.close).props("outline").classes("shadow-lg")
-        ui.button("Really Quit", icon="logout", on_click=cleanup_and_exit).props("outline").classes("shadow-lg")
-    
+        ui.label(
+            "You can safely close this window and analysis will keep running in the background."
+        )
+        ui.button("Cancel", on_click=quitdialog.close).props("outline").classes(
+            "shadow-lg"
+        )
+        ui.button("Really Quit", icon="logout", on_click=cleanup_and_exit).props(
+            "outline"
+        ).classes("shadow-lg")
+
     # Create a header with navigation title and menu
     with ui.header(elevated=True).classes("items-center duration-200 p-0 px-4 no-wrap"):
         with ui.grid(columns=2).style("width: 100%"):
-            ui.html(navtitle).classes("shadows-into").style("color: #FFFFFF; font-size: 200%; font-weight: 150").tailwind("drop-shadow", "font-bold")
+            ui.html(navtitle).classes("shadows-into").style(
+                "color: #FFFFFF; font-size: 200%; font-weight: 150"
+            ).tailwind("drop-shadow", "font-bold")
             with ui.row().classes("ml-auto align-top"):
                 with ui.button(icon="menu"):
                     with ui.menu() as menu:
                         ui.menu_item("Home", lambda: ui.navigate.to("/"))
                         ui.menu_item("Live Data", lambda: ui.navigate.to("/live"))
-                        ui.menu_item("Browse Historic Data", lambda: ui.navigate.to("/browse"))
+                        ui.menu_item(
+                            "Browse Historic Data", lambda: ui.navigate.to("/browse")
+                        )
                         ui.separator()
+                        ui.switch('allow remote access', value=False, on_change=use_on_air).classes(
+                            "ml-4 bg-transparent"
+                        ).props('color="black"')
+                        ui.switch("Dark Mode", on_change=dark_mode).classes(
+                            "ml-4 bg-transparent"
+                        ).props('color="black"')
                         ui.separator()
                         ui.menu_item("Close", menu.close)
                 ui.image(IMAGEFILE).style("width: 50px")
@@ -64,12 +83,23 @@ def frame(navtitle: str):
     # Create a footer with useful information and quit button
     with ui.footer().style("background-color: #4F9153"):
         with ui.dialog() as dialog, ui.card():
-            ui.label("Useful Information.").tailwind("text-2xl font-bold font-italic drop-shadow")
+            ui.label("Useful Information.").tailwind(
+                "text-2xl font-bold font-italic drop-shadow"
+            )
             ui.separator()
             ui.link("Code on GitHub", "https://github.com/looselab/cnsmeth")
-            ui.link("Rapid CNS2 Paper", "https://link.springer.com/article/10.1007/s00401-022-02415-6")
-            ui.link("Sturgeon Classifier", "https://www.nature.com/articles/s41586-023-06615-2")
-            ui.link("Protocol", "https://www.protocols.io/view/intra-operative-nanopore-sequencing-to-classify-br-c65qzg5w")
+            ui.link(
+                "Rapid CNS2 Paper",
+                "https://link.springer.com/article/10.1007/s00401-022-02415-6",
+            )
+            ui.link(
+                "Sturgeon Classifier",
+                "https://www.nature.com/articles/s41586-023-06615-2",
+            )
+            ui.link(
+                "Protocol",
+                "https://www.protocols.io/view/intra-operative-nanopore-sequencing-to-classify-br-c65qzg5w",
+            )
             ui.link("Oxford Nanopore", "https://nanoporetech.com/")
             ui.link("epi2me labs", "https://labs.epi2me.io/")
             ui.link("Looselab", "https://looselab.github.io/")
@@ -78,34 +108,40 @@ def frame(navtitle: str):
         ui.colors(primary="#555")
         ui.button("More Information", on_click=dialog.open)
         ui.button("Quit", icon="logout", on_click=quitdialog.open)
-        ui.label().bind_text_from(app, "urls", backward=lambda n: f"Available urls: {n}")
-        ui.label("Some aspects of this application are ©Looselab - all analyses provided for research use only.").tailwind("text-sm font-italic")
-    
+        ui.label().bind_text_from(
+            app, "urls", backward=lambda n: f"Available urls: {n}"
+        )
+        ui.label(
+            "Some aspects of this application are ©Looselab - all analyses provided for research use only."
+        ).tailwind("text-sm font-italic")
+
     yield
+
 
 def cleanup_and_exit():
     """
     Handle any necessary cleanup operations before exiting the application and then shut down the application.
-    
+
     Returns:
         None
-    
+
     Example:
         >>> cleanup_and_exit()
         None
     """
     app.shutdown()
 
+
 def dark_mode(event: events.ValueChangeEventArguments):
     """
     Toggle dark mode for the interface based on the value of the event argument.
-    
+
     Args:
         event (events.ValueChangeEventArguments): The event argument containing the value for dark mode toggle.
-    
+
     Returns:
         None
-    
+
     Example:
         >>> event = events.ValueChangeEventArguments(value=True)
         >>> dark_mode(event)
@@ -116,16 +152,17 @@ def dark_mode(event: events.ValueChangeEventArguments):
     else:
         ui.dark_mode().disable()
 
+
 def use_on_air(args: events.ValueChangeEventArguments):
     """
     Enable or disable remote access based on the value of the event argument.
-    
+
     Args:
         args (events.ValueChangeEventArguments): The event argument containing the value for remote access toggle.
-    
+
     Returns:
         None
-    
+
     Example:
         >>> args = events.ValueChangeEventArguments(value=True)
         >>> use_on_air(args)
