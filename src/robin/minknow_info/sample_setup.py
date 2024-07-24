1fd0ea7a810d7252fa4e191384b65eab8b7d6f96
mattloose <matt.loose@nottingham.ac.uk>
2024-07-24 22:39:29 +0100

    Fixing stepper

--- src/robin/minknow_info/sample_setup.py
+++ src/robin/minknow_info/sample_setup.py
@@ -205,7 +205,7 @@ class MinKNOWFish:
         # position = "1B"
         kit = "SQK-RAD114"
         ###Memo to self... basecall config must not include cfg
-        basecall_config = ["dna_r10.4.1_e8.2_400bps_5khz_modbases_5hmc_5mc_cg_hac_prom.cfg"]
+        basecall_config = "dna_r10.4.1_e8.2_400bps_5khz_modbases_5hmc_5mc_cg_hac_prom.cfg"
         alignment_reference = reference
         bed_file = "/home/deepseq/panel_adaptive_nogenenames_20122021_hg38.bed"
         experiment_duration = 24
@@ -298,12 +298,21 @@ class MinKNOWFish:
             )
 
 
+class ErrorChecker:
+    def __init__(self, *elements) -> None:
+        self.elements = elements
+
+    @property
+    def no_errors(self) -> bool:
+        return all(validation(element.value) for element in self.elements for validation in
+                   element.validation.values())
+
 @ui.page("/")
 async def content():
-    with theme.frame(
+    with ((theme.frame(
         "<strong><font color='#000000'>R</font></strong>apid nanop<strong><font color='#000000'>O</font></strong>re <strong><font color='#000000'>B</font></strong>rain intraoperat<strong><font color='#000000'>I</font></strong>ve classificatio<strong><font color='#000000'>N</font></strong>",
         smalltitle="<strong><font color='#000000'>R.O.B.I.N</font></strong>",
-    ):
+    ))):
         sample_camera = Camera(
             icon="photo_camera",
             icon_color="blue-5",
@@ -312,7 +321,6 @@ async def content():
             canvas_id="samplecanvas",
             on_change=lambda: generate_sampleID(),
         )
-
         async def generate_sampleID():
             await asyncio.sleep(0.1)
             print("calling camera sample")
@@ -370,47 +378,52 @@ async def content():
                     )
                     sampleid = ui.input(
                         placeholder="start typing",
-                        validation=lambda value: (
-                            "Too short" if len(value) < 5 else None
-                        ),
+                        validation = {'Too short': lambda value: len(value) >= 5}
                     ).props("rounded outlined dense")
                     sample_camera.show_camera()
                     with ui.stepper_navigation():
-                        ui.button("Next", on_click=stepper.next)
+                        checker = ErrorChecker(sampleid)
+                        c = ui.button("Next", on_click=stepper.next).bind_enabled_from(checker, 'no_errors')
+
                 with ui.step("Flowcell"):
                     ui.label("Enter the flowcell ID.")
                     flowcellid = ui.input(
                         placeholder="start typing",
-                        validation=lambda value: (
-                            "Not a valid flowcell ID"
-                            if not re.match(r"^[A-Za-z]{3}\d{5}$", value)
-                            else None
-                        ),
+                        validation = {'Not a valid flowcell ID': lambda value: re.match(r"^[A-Za-z]{3}\d{5}$", value) is not None}
                     ).props("rounded outlined dense")
                     flowcell_camera.show_camera()
                     with ui.stepper_navigation():
                         ui.button("Back", on_click=stepper.previous).props("flat")
-                        ui.button("Next", on_click=stepper.next)
+                        checkerflowcell = ErrorChecker(flowcellid)
+                        d=ui.button("Next", on_click=stepper.next).bind_enabled_from(checkerflowcell, 'no_errors')
                 with ui.step("Device Position"):
                     ui.label("Select the device position to be used.")
+                    run_button = ui.button(
+                        "Start Run",
+                        on_click=lambda: minknow.start_run(
+                            position=position.value.description.name,
+                            reference="/home/deepseq/refs/hg38_simple.fa",
+                            sample_id=sampleid.value,
+                            flowcell_id=flowcellid.value,
+                        ),
+                    )
+                    def dostuff():
+                        print("dostuff")
+                        run_button.enable()
                     position = ui.radio(
                         {
                             item: str(item)
                             for index, item in enumerate(minknow.positions)
                         },
-                        # on_change=minknow.enable_access,
-                    )  # .bind_value(self, "selected_position").props("inline")
-                    with ui.stepper_navigation():
+                    ).on(
+                        "update:model-value", dostuff
+                    )
+                    finalstep = ui.stepper_navigation()
+                    with finalstep:
                         ui.button("Back", on_click=stepper.previous).props("flat")
-                        ui.button(
-                            "Start Run",
-                            on_click=lambda: minknow.start_run(
-                                position=position.value.description.name,
-                                reference="/home/deepseq/refs/hg38_simple.fa",
-                                sample_id=sampleid.value,
-                                flowcell_id=flowcellid.value,
-                            ),
-                        )
+                        run_button.move(finalstep)
+                        run_button.disable()
+
             ui.separator()
             ui.label("Sample Settings:").style(
                 "color: #6E93D6; font-size: 100%; font-weight: 300"
@@ -462,169 +475,3 @@ def main():
 
 if __name__ in {"__main__", "__mp_main__"}:
     main()
-
-"""
-
-import logging
-from pathlib import Path
-import sys
-
-# minknow_api.manager supplies "Manager" a wrapper around MinKNOW's Manager gRPC API with utilities
-# for querying sequencing positions + offline basecalling tools.
-from enum import Enum
-from typing import Sequence
-
-from minknow_api.examples.load_sample_sheet import (
-    ParsedSampleSheetEntry,
-    SampleSheetParseError,
-    load_sample_sheet_csv,
-)
-from minknow_api.manager import Manager
-
-# We need `find_protocol` to search for the required protocol given a kit + product code.
-from minknow_api.tools import protocols
-
-class ExperimentSpec(object):
-    def __init__(self, position):
-        self.position = position
-        self.protocol_id = ""
-
-
-ExperimentSpecs = Sequence[ExperimentSpec]
-
-
-# Determine which protocol to run for each experiment, and add its ID to experiment_specs
-def add_protocol_ids(experiment_specs, kit, basecall_config):
-    for spec in experiment_specs:
-        # Connect to the sequencing position:
-        position_connection = spec.position.connect()
-
-        # Check if a flowcell is available for sequencing
-        flow_cell_info = position_connection.device.get_flow_cell_info()
-        if not flow_cell_info.has_flow_cell:
-            print("No flow cell present in position {}".format(spec.position))
-            sys.exit(1)
-
-        product_code = flow_cell_info.user_specified_product_code
-        if not product_code:
-            product_code = flow_cell_info.product_code
-
-        # Find the protocol identifier for the required protocol:
-        protocol_info = protocols.find_protocol(
-            position_connection,
-            product_code=product_code,
-            kit=kit,
-            basecalling=True,
-            basecall_config=basecall_config,
-            barcoding=False,
-            barcoding_kits=None,
-        )
-
-        if not protocol_info:
-            print("Failed to find protocol for position %s" % (spec.position))
-            print("Requested protocol:")
-            print("  product-code: %s" % product_code)
-            print("  kit: %s" % kit)
-            sys.exit(1)
-
-        # Store the identifier for later:
-        spec.protocol_id = protocol_info.identifier
-
-
-def main():
-    position = "1B"
-    kit = "SQK-RAD114"
-    basecall_config = "dna_r10.4.1_e8.2_400bps_5khz_modbases_5hmc_5mc_cg_hac.cfg"
-    alignment_reference = "/data/lambda.fasta"
-    bed_file = "/data/test.bed"
-    experiment_duration = 72
-    experiment_group_id = "experiment_group"
-    sample_id = "SAMPLE_ID"
-
-    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-
-    # Construct a manager using the host + port provided:
-    manager = Manager(
-        host="localhost",
-        port=None,
-    )
-
-    experiment_specs = []
-    # Add all the positions to the list:
-    for pos in manager.flow_cell_positions():
-        if pos.name == position:
-            experiment_specs.append(ExperimentSpec(position=pos))
-    add_protocol_ids(experiment_specs, kit, basecall_config)
-
-    # Build arguments for starting protocol:
-    alignment_args = protocols.AlignmentArgs(
-        reference_files=[alignment_reference],
-        bed_file=bed_file,
-    )
-
-    basecalling_args = protocols.BasecallingArgs(
-        config=basecall_config,
-        barcoding=None,
-        alignment=alignment_args,
-    )
-
-    read_until_args = protocols.ReadUntilArgs(
-        filter_type="enrich",
-        reference_files=[alignment_reference],
-        bed_file=bed_file,
-        first_channel=None,
-        last_channel=None
-    )
-
-    bam_arguments = protocols.OutputArgs(
-        reads_per_file=None, # 4000,
-        batch_duration="60",
-    )
-
-    # Now start the protocol(s):
-    print("Starting protocol on %s positions" % len(experiment_specs))
-    for spec in experiment_specs:
-        position_connection = spec.position.connect()
-
-        # Generate stop criteria for use by Run Until
-        # The `runtime` is in seconds, while the `experiment_duration` is in hours
-        stop_criteria = protocols.CriteriaValues(
-            runtime=int(experiment_duration * 60 * 60)
-        )
-
-        run_id = protocols.start_protocol(
-            position_connection,
-            identifier=spec.protocol_id,
-            sample_id=sample_id,
-            experiment_group=experiment_group_id,
-            barcode_info=None,
-            basecalling=basecalling_args,
-            read_until=read_until_args,
-            fastq_arguments=None,
-            fast5_arguments=None,
-            pod5_arguments=None,
-            bam_arguments=bam_arguments,
-            disable_active_channel_selection=False,
-            mux_scan_period=1.5,
-            stop_criteria=stop_criteria,
-            args=None,  # Any extra args passed.
-        )
-
-        flow_cell_info = position_connection.device.get_flow_cell_info()
-
-        print("Started protocol:")
-        print("    run_id={}".format(run_id))
-        print("    position={}".format(spec.position.name))
-        print("    flow_cell_id={}".format(flow_cell_info.flow_cell_id))
-        print(
-            "    user_specified_flow_cell_id={}".format(
-                flow_cell_info.user_specified_flow_cell_id
-            )
-        )
-
-
-if __name__ == "__main__":
-    main()
-
-
-"""
