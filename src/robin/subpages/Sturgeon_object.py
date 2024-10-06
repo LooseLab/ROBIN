5b7e8a705d939e8479ccb54969cb4336065e16f8
mattloose <matt.loose@nottingham.ac.uk>
2024-10-06 21:39:28 +0100

    dark mode fixes

--- src/robin/subpages/Sturgeon_object.py
+++ src/robin/subpages/Sturgeon_object.py
@@ -87,15 +87,15 @@ class Sturgeon_object(BaseAnalysis):
         super().__init__(*args, **kwargs)
 
     def setup_ui(self):
-        self.card = ui.card().style("width: 100%")
+        self.card = ui.card().classes('dark:bg-black').style("width: 100%")
         with self.card:
             with ui.grid(columns=8).classes("w-full h-auto"):
                 with ui.card().classes(
-                    f"min-[{self.MENU_BREAKPOINT+1}px]:col-span-3 max-[{self.MENU_BREAKPOINT}px]:col-span-8"
+                    f"min-[{self.MENU_BREAKPOINT+1}px]:col-span-3 max-[{self.MENU_BREAKPOINT}px]:col-span-8 dark:bg-black"
                 ):
                     self.create_sturgeon_chart("Sturgeon")
                 with ui.card().classes(
-                    f"min-[{self.MENU_BREAKPOINT+1}px]:col-span-5 max-[{self.MENU_BREAKPOINT}px]:col-span-8"
+                    f"min-[{self.MENU_BREAKPOINT+1}px]:col-span-5 max-[{self.MENU_BREAKPOINT}px]:col-span-8 dark:bg-black"
                 ):
                     self.create_sturgeon_time_chart("Sturgeon Time Series")
         if self.summary:
