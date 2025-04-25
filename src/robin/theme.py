b1e83f7750ab794fe0570ab7ab1289b681eebbfb
mattloose <matt.loose@nottingham.ac.uk>
2025-04-25 17:46:45 +0100

    Update theme.py
    Remove misplaced menu.

--- src/robin/theme.py
+++ src/robin/theme.py
@@ -522,17 +522,6 @@ def frame(navtitle: str, batphone=False, smalltitle=None):
         ui.label("Not for diagnostic use.").classes(
             f"min-[{MENU_BREAKPOINT+1}px]:hidden"
         ).tailwind("text-sm font-italic")
-        
-        result = ui.label().classes('mr-auto')
-        with ui.button(icon='menu'):
-            with ui.menu() as menu:
-                ui.menu_item('Menu item 1', lambda: result.set_text('Selected item 1'))
-                ui.menu_item('Menu item 2', lambda: result.set_text('Selected item 2'))
-                ui.menu_item('Menu item 3 (keep open)',
-                            lambda: result.set_text('Selected item 3'), auto_close=False)
-                ui.separator()
-                ui.menu_item('Close', menu.close)
-
 
     yield
 
