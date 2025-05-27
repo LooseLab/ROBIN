bd841e32790046559d066395131fe0665584fb48
mattloose <matt.loose@nottingham.ac.uk>
2025-05-27 17:01:36 +0100

    Version Bump

--- src/robin/__about__.py
+++ src/robin/__about__.py
@@ -1,4 +1,5 @@
 # SPDX-FileCopyrightText: 2023-present Matt Loose <matt.loose@nottingham.ac.uk>
 #
 # SPDX-License-Identifier: MIT
-__version__ = "0.2.0"
+# Version for performance testing.
+__version__ = "0.2.1"
