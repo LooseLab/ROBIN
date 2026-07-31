38dcacea140af28a17e554286d9c1b617b8af7d1
mattloose <matt.loose@nottingham.ac.uk>
2026-07-31 17:25:27 +0100

    Fix corrupt file.

--- src/robin/gui/components/cnv.py
+++ src/robin/gui/components/cnv.py
@@ -1,4 +1,4 @@
-src/robin/gui/components/cnv.pyfrom __future__ import annotations
+from __future__ import annotations
 
 from typing import Any, Dict, List, Optional, Sequence, Tuple
 from pathlib import Path
